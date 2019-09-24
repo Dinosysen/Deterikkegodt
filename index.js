f5ed36cb1f46c57935513e9b38bb9aa64c77ee73
ZwizyMan <54938955+ZwizyMan@users.noreply.github.com>
2019-09-24 14:02:29 +0200

    Hovedside forste utkast

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-var http = require("http");
+let http = require("http");
 
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write('URL er: "' + req.url + '"');
+let handleRequest = (request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  let url = request.url;
+  if (url == "/") {
+  } else if (url == "/sparkesykler") {
+    lastfil(response, "sparkesykler.html");
+  } else if (url == "/havfruer") {
+    lastfil(response, "havfruer.html");
+  } else if (url == "/biler") {
+    lastfil(response, "biler.html");
+  } else {
+    response.write("<h1>Ikke implementert</h1>");
+  }
+};
 
-    let url = req.url;
-    if (url == "/") {
-      {
-        blogginlegg(res);
-      }
-    } else if (url == "/about") {
-      about(res);
+http.createServer(handleRequest).listen(8080);
+
+function lastfil(response, filnavn) {
+  let fs = require("fs");
+  fs.readFile(filnavn, null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      respone.write("Whoops! File not found!");
     } else {
-      res.write("<h1>Ikke implementert</h1>");
+      response.write(data);
     }
-    // res.write("Hello World");
-    res.end("slutt!");
-  })
-  .listen(8080);
-
+    response.end();
+  });
+}
 function about(res) {
   res.write("About this page");
 }
