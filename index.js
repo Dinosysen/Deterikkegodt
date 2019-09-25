e42cd7be3b8c55d449f0190376faee86ba9d54a9
stealthalgorithm <joakim@polden.mobi>
2019-09-25 10:14:20 +0200

    fikset css for index js

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 let http = require("http");
 
 let handleRequest = (request, response) => {
-  response.writeHead(200, {
-    "Content-Type": "text/html"
-  });
-  let url = request.url;
-  if (url == "/") {
-    lastfil(response, "index.html");
-  } else if (url == "/sparkesykler") {
-    lastfil(response, "sparkesykler.html");
-  } else if (url == "/havfruer") {
-    lastfil(response, "havfruer.html");
-  } else if (url == "/biler") {
-    lastfil(response, "biler.html");
-  } else if (url == "/checkout") {
-    lastfil(response, "Checkout/checkout.html");
-  } else if (url == "/hage") {
-    lastfil(response, "Hage/hage.html");
-  } else if (url == "/kontakt") {
-    lastfil(response, "Kontakt/index.html");
-  } else if (url == "/mennesker") {
-    lastfil(response, "mennesker/index.html");
-  } else if (url == "/olje") {
-    lastfil(response, "olje/olje.html");
-  } else if (url == "/omoss") {
-    lastfil(response, "Omoss/omoss.html");
-  } else {
-    response.write("<h1>Ikke implementert</h1>");
+  switch (request.url) {
+    case "/css/main.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      lastfil(response, "css/main.css");
+      break;
+    default:
+      response.writeHead(200, { "Content-Type": "text/html" });
+
+      let url = request.url;
+      if (url == "/") {
+        lastfil(response, "index.html");
+      } else if (url == "/sparkesykler") {
+        lastfil(response, "sparkesykler.html");
+      } else if (url == "/havfruer") {
+        lastfil(response, "havfruer.html");
+      } else if (url == "/biler") {
+        lastfil(response, "biler.html");
+      } else if (url == "/checkout") {
+        lastfil(response, "Checkout/checkout.html");
+      } else if (url == "/hage") {
+        lastfil(response, "Hage/hage.html");
+      } else if (url == "/kontakt") {
+        lastfil(response, "Kontakt/index.html");
+      } else if (url == "/mennesker") {
+        lastfil(response, "mennesker/index.html");
+      } else if (url == "/olje") {
+        lastfil(response, "olje/olje.html");
+      } else if (url == "/omoss") {
+        lastfil(response, "Omoss/omoss.html");
+      } else {
+        response.write("<h1>Ikke implementert</h1>");
+      }
   }
 };
-
 http.createServer(handleRequest).listen(8080);
 
 function lastfil(response, filnavn) {
